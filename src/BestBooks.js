8ac109e60699977b678247d04d30bc76403041c6
marwanrawshedh <marwansalem55@yahoo.com>
2021-09-13 20:02:36 +0300

    lab11

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -5,25 +5,54 @@ import './BestBooks.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios'
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      book: []
+    }
+  }
+  componentDidMount = () => {
+    const { user } = this.props.auth0;
+    let email = user.email
+    let url = `http://localhost:3010/books?frontemail=${email}`
+    axios.
+      get(url).
+      then(result => {
+        this.setState({
+          book: result.data
+        })
+        console.log(result.data)
+
+      })
+  }
   render() {
-    return(
-      <Jumbotron>
-        <Card style={{ width: '18rem' }}>
-  <Card.Header>My Favorite Books</Card.Header>
-  <ListGroup variant="flush">
-    <ListGroup.Item>al resala</ListGroup.Item>
-    <ListGroup.Item>al mosnad</ListGroup.Item>
-    <ListGroup.Item>al moata`a</ListGroup.Item>
-  </ListGroup>
-</Card>
-        {/* <h1>My Favorite Books</h1> */}
-        {/* <p>
+
+    return (
+
+      <>
+        <h1>My Favorite Books</h1> 
+        <p>
           This is a collection of my favorite books
-        </p> */}
-      </Jumbotron>
+        </p> 
+
+        {this.state.book.map(item => {return(
+          <Jumbotron>
+            <Card style={{ width: '18rem' }}>
+              <Card.Header>name book :{item.title}</Card.Header>
+              <ListGroup variant="flush">
+                <ListGroup.Item>book sescription :{item.description}</ListGroup.Item>
+                <ListGroup.Item>book status : {item.status}</ListGroup.Item>
+                
+              </ListGroup>
+            </Card>
+          </Jumbotron>
+        )})}
+      </>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
